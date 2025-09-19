f67ac49e69078559e1f24df6f627b00c88b0042a
Lexie Scholtz <lexiescholtz@gmail.com>
2025-09-18 20:46:45 -0600

    initial version - stacked raw vs calibrated data

---
+++ map_dyna.py
@@ -0,0 +1,104 @@
+# plot the raw Dynabeads data for thesis
+#
+# Lexie Scholtz
+# Created 2025.09.18
+
+ver = 1.0
+to_save = True
+
+import sys
+import numpy as np
+from matplotlib import pyplot as plt
+from qc_formats import *
+from matplotlib import gridspec as gridspec
+
+# laser warmup
+path = '../../../../../armani_lab/research/qcMAP/qc_data/'
+
+# label, dir, file_prefix, ws, xs, ys, zs
+df = ['1', '2', '3'] # default
+files = [
+    ['M-280 Streptavidin lot 1', '2025.04.28/dm81_2', df, df, df, df],
+    ['M-280 Streptavidin lot 2', '2025.04.28/dm82_2', df, df, df, df],
+    ['M-280 Streptavidin lot 3', '2025.04.28/dm83_2', df, df, df, ['4', '5', '6']],
+    ['M-270 Streptavidin', '2025.04.29/dm7_2', df, df, df, df],
+    ['MyOne C1 Streptavidin', '2025.04.28/dc1_2', df, df, df, df],
+    ['MyOne T1 Streptavidin', '2025.04.29/dt1_2', df, df, df, df],
+    ['Carboxylic Acid', '2025.04.29/dca_2', df, df, df, df],
+    ['Protein G', '2025.05.01/dpg_2', df, ['1-ex', '2-ex', '3-ex'], df, df]
+]
+dm83z_dir = '2025.04.28/dm83_2'
+
+c_ids = ['w', 'x', 'y', 'z']
+cs = [100, 80, 60, 40]
+
+colors = [purple, cyan, orange, pink]
+
+fig = plt.figure(figsize=(6.5, 6), dpi=dpi_disp)
+gs = gridspec.GridSpec(5, 4, height_ratios=[0.1, 1, 1, 1, 1])
+leg_ax = fig.add_subplot(gs[0, :])
+prep_legax([leg_ax])
+
+axes = []
+conc_axes = []
+for r in range(2):
+    for c in range(4):
+        axes.append(fig.add_subplot(gs[r*2 + 1, c]))
+        conc_axes.append(fig.add_subplot(gs[r*2 + 2, c]))
+        # conc_axes.append(axes[-1].inset_axes([0.3, 0.15, 0.6, 0.6]))
+
+for i in range(len(files)):
+    ax = axes[i]
+    conc_ax = conc_axes[i]
+
+    series = files[i]
+    series_name = series[0]
+    dir = series[1]
+    suffixes = series[2:]
+
+    for ci in range(len(suffixes)):
+        conc_series = suffixes[ci]
+        print('{}{}'.format(dir, conc_series))
+        c_id = c_ids[ci]
+        c0 = cs[ci]
+
+        if 'lot 3' in series_name and ci == 3:
+            dir = dm83z_dir
+
+        for fi in range(len(conc_series)):
+            data = np.genfromtxt(path + dir + c_id + conc_series[fi] + '.txt')
+
+            time = data[:, 0] / 60 # convert to min
+            lux = data[:, 1] / 1000 # convert to klx
+
+            ax.plot(time, lux, color=colors[ci], alpha=1-fi/3)
+
+            conc = calibrate(lux, c0)
+            conc_ax.plot(time, conc, color=colors[ci], alpha=1-fi/3)
+
+for ax in axes:
+    ax.set_xlabel('Time (min)')
+    ax.set_ylabel('Illum. (klx)')
+    ax.set_xlim([0, 5])
+    ax.set_ylim([-2, 32])
+
+for cax in conc_axes:
+    cax.set_xlabel('Time (min)')
+    cax.set_ylabel('Conc. (µg/mL)')
+    cax.set_xlim([0, 5])
+    cax.set_ylim([-10, 110])
+
+
+# --- SAVE FIG ---
+plt.tight_layout(pad=pad_tight)
+
+img_name = '../subgraphics/map_dyna_' + str(ver) + '.png'
+
+if not to_save:
+    # show fig - previews only
+    print('displaying ' + img_name)
+    plt.show()
+else:
+    # save fig
+    plt.savefig(img_name, dpi=dpi_save)
+    print('saved file as: ' + img_name)
